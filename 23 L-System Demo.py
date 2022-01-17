c484e211358ef10bd97f40c29ec80c8ffd865b80
stefan-scott <scottst@spsd.sk.ca>
2022-01-17 10:25:11 -0600

    L-System WIP

---
+++ 23 L-System Demo.py
@@ -0,0 +1,49 @@
+# L-System Demo
+# Mr. Scott
+# Jan 17, 2022
+# Some complex Turtle Graphics
+
+def apply_rules(ch):
+    #Apply rules to a single character
+    if ch == "A":
+        return "B"
+    elif ch == "B":
+        return "AB"
+    else:
+        return ch
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# warm-up: Accumulator pattern
+
+# def my_mult(a, b):  #4, 100  (400)
+#     # calculate multiplication through
+#     # repeated addition
+#     #result = 0  #accumulator variable
+#     result = 0
+#     for i in range(b):
+#         result += a
+#     return result
+        
+        
+        
+        
+        
+        
+        
+    
+    
